d8bd46c062b73c9c361cbb2658b6ebf96653186d
GUYL Bastien <bastien@guyl.me>
2016-12-12 17:29:50 +0100

    Add agent

--- src/com/guylplatteau/sma/puzzle/model/Agent.java
+++ src/com/guylplatteau/sma/puzzle/model/Agent.java
@@ -1,7 +1,26 @@
-package com.guylplatteau.sma.puzzle;
+package com.guylplatteau.sma.puzzle.model;
 
-/**
- * Created by vilgh on 12/12/2016.
- */
-public class Agent {
+import java.awt.Point;
+import java.util.List;
+
+
+public class Agent extends Thread {
+
+    private static final int GRID_WIDTH = 5, GRID_HEIGHT = 5;
+    private static Grid grid = new Grid(GRID_WIDTH, GRID_HEIGHT);
+
+    private Point currentPosition, destination;
+    private List<Agent> neighbors;
+
+    Agent(Point position, Point destination) {
+        this.currentPosition = position;
+        this.destination = destination;
+        grid.add(this);
+        neighbors = grid.getNeighbors(this);
+    }
+
+    Agent(int x, int y, int destination_x, int destination_y){
+        this(new Point(x,y), new Point(destination_x, destination_y));
+    }
+
 }
